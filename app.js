4e199fc0c10c50af6e25ad9a31837cc56aa4bfeb
Sean Prasertsit <sean@wulfcode.com>
2017-05-02 19:26:24 -0700

    :sparkles: Added server and index

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, '/templates'));
+
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!' })
+});
+
+app.listen(3001, function () {
+  console.log('Example app listening on port 3001!')
+})
